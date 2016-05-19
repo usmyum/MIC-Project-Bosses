121a645733a5f9c1a8622acdb6d7d3a50b219df2
usmyum <usmyum@gmail.com>
2016-05-19 15:27:23 +0500

    Uploading the whole project

---
+++ MICProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MICProject.MvcApplication" Language="C#" %>
---
+++ MICProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MICProject.Startup))]
+namespace MICProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
